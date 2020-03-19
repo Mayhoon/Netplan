ed339404b294ac4e31a7b35eb5e8380dcb8aae91
Mayhoon <fell2ratte@t-online.de>
2020-03-19 17:39:34 +0100

    Refactor the code

--- src/main/Netzplan.java
+++ src/main/Netzplan.java
@@ -8,136 +8,144 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Netzplan {
+    public static final String GREEN = "\u001B[32m";
+    public static final String COLOR_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         Netzplan netzplan = new Netzplan();
-        netzplan.getNodes();
-        netzplan.validate();
+        netzplan.getInput();
+        //netzplan.validate();
         netzplan.createNet();
         netzplan.fillNet();
         netzplan.iterateToEnd();
         netzplan.iterateToStart();
-        netzplan.calcGP();
-        netzplan.calcFP();
-        netzplan.getPath();
+        netzplan.calculateOptimalPath();
     }
 
     List<Node> net;
-    List<String> allNodeNames;
-    List<Float> allNodeProcessingTimes;
-    List<String> allpreviousNodeNames;
-    List<String> allnextNodeNames;
+    List<String> inputs_NodeNames;
+    List<Float> inputs_ProcessingTime;
+    List<String> inputs_PreviousNodes;
+    List<String> inputs_FollowingNodes;
 
     Node startingNode;
     Scanner scanner;
-    Input givenInput;
+    InputType inputType;
 
     public Netzplan() {
         scanner = new Scanner(System.in);
         net = new ArrayList<>();
-        allNodeNames = new ArrayList<>();
-        allNodeProcessingTimes = new ArrayList<>();
-        allpreviousNodeNames = new ArrayList<>();
+        inputs_NodeNames = new ArrayList<>();
+        inputs_ProcessingTime = new ArrayList<>();
+        inputs_PreviousNodes = new ArrayList<>();
     }
 
-    private void getNodes() {
-        System.out.print("Do you know the previous or following node? \n(prev / foll):\t\t");
-        String input;
+    private void getInput() {
         try {
-            input = scanner.next();
-            if (input.equals("prev")) {
-                givenInput = Input.PREVIOUS_NODES;
-            } else if (input.equals("foll")) {
-                givenInput = Input.NEXT_NODES;
-            } else {
-                System.out.println("Invalid Input.");
-                System.exit(0);
-            }
-            System.out.println("------------------");
-
-            while (true) {
-                while (true) {
-                    System.out.print("Node name:\t\t");
-                    input = scanner.next();
-                    if (!allNodeNames.contains(input)) {
-                        allNodeNames.add(input);
-                        break;
-                    } else {
-                        System.out.println("Node name already exists. Please try again:");
-                    }
-                }
-
-                // Get the porcessing time
-                System.out.print("Process time:\t\t");
-                allNodeProcessingTimes.add(scanner.nextFloat());
-
-                // Get the name of either the following or the previous node
-                if (givenInput == Input.PREVIOUS_NODES) {
-                    System.out.print("Previous nodes\n(Ex: A,B):\t\t");
-                    allpreviousNodeNames.add(scanner.next());
-
-                } else if (givenInput == Input.NEXT_NODES) {
-                    System.out.print("Following nodes\n(Ex: A,B):\t\t");
-                    allnextNodeNames.add(scanner.next());
-                }
-
-                // Check wether the user wants to add another node to the net or not
-                System.out.print("Add another one? y/n\t");
-                input = scanner.next();
-                if (input.equals("n")) {
-                    break;
-                } else if (input.equals("y")) {
-                    System.out.println("------------------");
-                }else {
-                    System.out.println("Invalid Input.");
-                    System.exit(0);
-                }
-            }
+            getInputType();
+            do {
+                inputNodeName();
+                inputProcessingTime();
+                inputRelatedNodes();
+            } while (askToRepeat());
         } catch (InputMismatchException e) {
             e.printStackTrace();
         }
     }
 
-    private void validate() {
-        System.out.println("-------------------");
-        String[] nodeNames;
+    private void getInputType() {
+        System.out.print("Do you know the previous or following node? \n(prev / foll):\t\t");
+        String input = scanner.next();
+        if (input.equals("prev")) {
+            inputType = InputType.PREVIOUS_NODES;
+        } else if (input.equals("foll")) {
+            inputType = InputType.NEXT_NODES;
+        } else {
+            System.out.println("Invalid Input.");
+            System.exit(0);
+        }
+        System.out.println("------------------");
+    }
 
-        for (int i = 0; i < allpreviousNodeNames.size(); i++) {
-            // Check if the current node is the starting node
-            if (allpreviousNodeNames.get(i).equals("0")) {
-                System.out.println("Starting node: " + allNodeNames.get(i));
-                startingNode = new Node(allNodeNames.get(i), allNodeProcessingTimes.get(i));
-                net.add(startingNode);
+    private void inputNodeName() {
+        while (true) {
+            System.out.print("Node name:\t\t\t");
+            String input = scanner.next();
+            if (!inputs_NodeNames.contains(input)) {
+                inputs_NodeNames.add(input);
+                break;
             } else {
-                // Check if the given previous nodes are valid
-                nodeNames = allpreviousNodeNames.get(i).split(",");
-                for (int d = 0; d < nodeNames.length; d++) {
-                    int occurrences = Collections.frequency(Arrays.asList(nodeNames), nodeNames[d]);
-                    if (occurrences > 1) {
-                        System.out.println(allNodeNames.get(i) + " contains " + occurrences + " duplicates. Exiting...");
-                        System.exit(0);
-                    }
-                }
+                System.out.println("Node name already exists. Please try again:");
             }
         }
-        System.out.println("-------------------");
-        System.out.println("The whole table is valid! Generate net...");
     }
+
+    private void inputProcessingTime() {
+        System.out.print("Process time:\t\t");
+        inputs_ProcessingTime.add(scanner.nextFloat());
+    }
+
+    private void inputRelatedNodes() {
+        if (inputType == InputType.PREVIOUS_NODES) {
+            System.out.print("Previous nodes\n(Ex: A,B):\t\t\t");
+            inputs_PreviousNodes.add(scanner.next());
+        } else if (inputType == InputType.NEXT_NODES) {
+            System.out.print("Following nodes\n(Ex: A,B):\t\t\t");
+            inputs_FollowingNodes.add(scanner.next());
+        }
+    }
+
+    // Check wether the user wants to add another node to the net or not
+    private boolean askToRepeat() {
+        System.out.print("Add another one? \n(y/n)\t\t\t\t");
+        String input = scanner.next();
+        if (input.equals("n")) {
+            return false;
+        } else if (input.equals("y")) {
+            System.out.println("------------------");
+            return true;
+        } else {
+            System.out.println("Invalid Input.");
+            System.exit(0);
+            return false;
+        }
+    }
+
+//    private void validate() {
+//        System.out.println("-------------------");
+//        String[] nodeNames;
+//
+//        for (int i = 0; i < inputs_PreviousNodes.size(); i++) {
+//            if (inputs_PreviousNodes.get(i).equals("0")) {
+//                nodeNames = inputs_PreviousNodes.get(i).split(",");
+//                for (String nodeName : nodeNames) {
+//                    int occurrences = Collections.frequency(Arrays.asList(nodeNames), nodeName);
+//                    if (occurrences > 1) {
+//                        System.out.println(inputs_NodeNames.get(i) + " contains " + occurrences + " duplicates. Exiting...");
+//                        System.exit(0);
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println("-------------------");
+//        System.out.println("The whole table is valid! Generate net...");
+//    }
 
     private void createNet() {
         System.out.println("-------------------");
-        
         // Check for the node that requires the starting node
-        for (int i = 0; i < allNodeNames.size(); i++) {
-            
-            String currentNodenName = allNodeNames.get(i);
+        for (int i = 0; i < inputs_NodeNames.size(); i++) {
+            if (inputs_PreviousNodes.get(i).equals("0")) {
+                System.out.println("Starting node: " + inputs_NodeNames.get(i));
+                startingNode = new Node(inputs_NodeNames.get(i), inputs_ProcessingTime.get(i));
+                net.add(startingNode);
+            }
+            String currentNodenName = inputs_NodeNames.get(i);
             // Ignore the starting node name
             if (!currentNodenName.equals(startingNode.name)) {
-                
                 // Check if the node is already initialized
-                boolean found = false;  
+                boolean found = false;
                 for (int netIndex = 0; netIndex < net.size(); netIndex++) {
-                    
                     // If so then do nothing 
                     if (net.get(netIndex).name.equals(currentNodenName)) {
                         //System.out.println("Node " + currentNodenName + " already in the net");
@@ -145,9 +153,7 @@ public class Netzplan {
                     }
                     // If not add it to the net
                     else if (netIndex == net.size() - 1 && !found) {
-                        // System.out.println("Node with the name " + currentNodenName
-                        //         + " not found in the net. Adding the node to the net...");
-                        Node node = new Node(currentNodenName, allNodeProcessingTimes.get(i));
+                        Node node = new Node(currentNodenName, inputs_ProcessingTime.get(i));
                         net.add(node);
                     }
                 }
@@ -158,14 +164,12 @@ public class Netzplan {
 
     private void fillNet() {
         System.out.println("-------------------");
-        System.out.println("\n\nNet plan:");
+        System.out.println(GREEN + "Net plan:" + COLOR_RESET);
         for (int i = 0; i < net.size(); i++) {
-            
             // Ignore the starting node
             if (!net.get(i).name.equals(startingNode.name)) {
-               
                 // Add the previous nodes
-                String[] previousNodeNames = allpreviousNodeNames.get(i).split(",");
+                String[] previousNodeNames = inputs_PreviousNodes.get(i).split(",");
                 for (int p = 0; p < previousNodeNames.length; p++) {
                     for (Node nodes : net) {
                         if (nodes.name.equals(previousNodeNames[p])) {
@@ -217,8 +221,14 @@ public class Netzplan {
             }
         }
     }
+
+    private void calculateOptimalPath() {
+        calcGP();
+        calcFP();
+        getPath();
+    }
 }
 
-enum Input {
+enum InputType {
     PREVIOUS_NODES, NEXT_NODES
 }
